e839a2597102d53eb9a88b8b2ca7f1760386f1ab
Rodrigo Avellar <roavellarm@gmail.com>
2020-04-23 03:44:30 -0300

    First commit

---
+++ src/config/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+function startServer(port, routes) {
+  if (!port) throw Error(`Port is required to start server`)
+  if (!routes) throw Error(`Routes is required to start server`)
+
+  const app = express()
+  app.use(express.json())
+  app.use('/', routes)
+  app.listen(port, () =>
+    // eslint-disable-next-line no-console
+    console.log(
+      `Server running at port ${port} in ${process.env.NODE_ENV} mode`
+    )
+  )
+}
+
+export default startServer
