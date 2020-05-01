98f22bfebcda277e72a8a19d452af8b315b20968
Gustavo <gustavosohne38@gmail.com>
2020-05-01 20:13:42 -0300

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,18 @@
 import { Router } from 'express'
 
-const routes = Router()
+const routes = new Router()
 
-const routesList = [routes.get('/', routes)]
+const routesList = [
+  routes.get('/', function (request, response) {
+    try {
+      const data = { title: 'Padma Yoga API', version: '0.0.0' }
+      return response.status(200).send(data)
+    } catch (error) {
+      return response
+        .status(500)
+        .send({ message: 'Falha ao processar sua requisição', error })
+    }
+  }),
+]
 
 export default routesList
